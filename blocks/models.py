5f7b015e51cf73a4502f756235e3139206083bf3
codeparameter <codeparameter.com@gmail.com>
2024-05-17 12:59:05 +0330

    block model migrated

--- blocks/models.py
+++ blocks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50)
+
+class Material(models.Model):
+    name = models.CharField(max_length=50)
+
+class Schema(models.Model):
+    name = models.CharField(max_length=50)
+
+class Quality(models.Model):
+    grade = models.CharField(max_length=50)
+
+class Block(models.Model):
+    city = models.ForeignKey("City", on_delete=models.CASCADE)
+    material = models.ForeignKey("Material", on_delete=models.CASCADE)
+    schema = models.ForeignKey("Schema", on_delete=models.CASCADE)
+    quality = models.ForeignKey("Quality", on_delete=models.CASCADE)
+    length = models.IntegerField()
+    height = models.IntegerField()
+    width = models.IntegerField()
+    available = models.BooleanField()
--- home/settings.py
+++ home/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # django_rest
 
     # my apps
+    'blocks',
 ]
 
 MIDDLEWARE = [
