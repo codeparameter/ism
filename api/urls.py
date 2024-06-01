50b6053c4a5edb4d6705b3ff76a1a566ef27d3cf
codeparameter <codeparameter.com@gmail.com>
2024-06-01 16:15:21 +0330

    validate token

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from . import routes
+from .views import ValidateToken
 
 urlpatterns = [
     path('auth/', obtain_auth_token),
+    path('validate-token/', ValidateToken.as_view()),
     path('', include(routes)),
 ]
