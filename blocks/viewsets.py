2613e8a3559bbcc5439e1456b583a497a237be7d
codeparameter <codeparameter.com@gmail.com>
2024-06-20 04:42:04 +0330

    removed unnecessary imports

--- blocks/viewsets.py
+++ blocks/viewsets.py
@@ -1,6 +1,5 @@
-from rest_framework import mixins, viewsets, status
+from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
-from rest_framework.response import Response
 from files.viewsets import add_pics_to_model_record
 from .serializers import *
 
