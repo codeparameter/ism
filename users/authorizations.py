679159a3345ee59acf69405bfbac5a204e26cf27
codeparameter <codeparameter.com@gmail.com>
2024-07-17 08:52:03 +0330

    error resolved so far, not checking for result yet

--- users/authorizations.py
+++ users/authorizations.py
@@ -3,35 +3,35 @@ from mines.models import Mine, MineStaff
 from factories.models import Factory, FactoryStaff
 
 
-# ADMIN_PERMISSIONS = [
-#     Permission.objects.get_or_create(
-#         defaults={"name": "Can verify mines",},
-#         content_type=ContentType(Mine),
-#         codename="verify_mines",
-#         )[0],
-#     Permission.objects.get_or_create(
-#         defaults={"name": "Can verify mine staffs",},
-#         content_type=ContentType(MineStaff),
-#         codename="verify_mine_staffs",
-#         )[0],
-#     Permission.objects.get_or_create(
-#         defaults={"name": "Can verify factories",},
-#         content_type=ContentType(Factory),
-#         codename="verify_factories",
-#         )[0],
-#     Permission.objects.get_or_create(
-#         defaults={"name": "Can verify factory staffs",},
-#         content_type=ContentType(FactoryStaff),
-#         codename="verify_factory_staffs",
-#         )[0],
-# ]
+ADMIN_PERMISSIONS = [
+    Permission.objects.get_or_create(
+        defaults={"name": "Can verify mines",},
+        content_type=ContentType.objects.get_for_model(Mine),
+        codename="verify_mines",
+        )[0],
+    Permission.objects.get_or_create(
+        defaults={"name": "Can verify mine staffs",},
+        content_type=ContentType.objects.get_for_model(MineStaff),
+        codename="verify_mine_staffs",
+        )[0],
+    Permission.objects.get_or_create(
+        defaults={"name": "Can verify factories",},
+        content_type=ContentType.objects.get_for_model(Factory),
+        codename="verify_factories",
+        )[0],
+    Permission.objects.get_or_create(
+        defaults={"name": "Can verify factory staffs",},
+        content_type=ContentType.objects.get_for_model(FactoryStaff),
+        codename="verify_factory_staffs",
+        )[0],
+]
 
 
 def authorize():
 
     admins, _ = Group.objects.get_or_create(name="Administrators")
 
-    # admins.permissions.set(ADMIN_PERMISSIONS)
+    admins.permissions.set(ADMIN_PERMISSIONS)
 
     for app in (
         'mines',
