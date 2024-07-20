ceea6ee36fa5b4bf2911da1b77b680743a3e4a34
codeparameter <codeparameter.com@gmail.com>
2024-07-20 06:03:00 +0330

    0002 phone dependency expire + v_code

--- phones/models.py
+++ phones/models.py
@@ -4,6 +4,8 @@ from django.core.validators import MinLengthValidator
 
 class Phone(models.Model):
     No = models.CharField('No', unique=True, max_length=10, validators=[MinLengthValidator(10)])
-    pre = models.CharField('pre', default='98', max_length=4, validators=[MinLengthValidator(1)]) 
+    pre = models.CharField('pre', default='98', max_length=4, validators=[MinLengthValidator(1)])
     is_mobile = models.BooleanField(default=True, blank=True)
-    dependency = models.JSONField(default=list, blank=True)
+    dependency = models.JSONField(default=dict, blank=True) # type content
+    v_code = models.CharField('v_code', max_length=4, validators=[MinLengthValidator(4)]) # verification code
+    expire = models.DateTimeField(null=True, blank=True)
