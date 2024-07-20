fdf8ab24e54e5728fa4bd804a0452a175cbb85b5
codeparameter <codeparameter.com@gmail.com>
2024-07-20 10:58:33 +0330

    003 pre + no uniqueness must validate together

--- phones/models.py
+++ phones/models.py
@@ -3,8 +3,9 @@ from django.core.validators import MinLengthValidator
 
 
 class Phone(models.Model):
-    No = models.CharField('No', unique=True, max_length=10, validators=[MinLengthValidator(10)])
+    No = models.CharField('No', max_length=10, validators=[MinLengthValidator(10)])
     pre = models.CharField('pre', default='98', max_length=4, validators=[MinLengthValidator(1)])
+    # pre + no uniqueness must validate together
     is_mobile = models.BooleanField(default=True, blank=True)
     dependency = models.JSONField(default=dict, blank=True) # type content
     v_code = models.CharField('v_code', max_length=4, validators=[MinLengthValidator(4)]) # verification code
