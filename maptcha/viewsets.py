a8d7f9700a1e16ff0fd0247a78d3704356745fae
codeparameter <codeparameter.com@gmail.com>
2024-07-23 02:52:01 +0330

    comment unused

--- maptcha/viewsets.py
+++ maptcha/viewsets.py
@@ -1,9 +1,9 @@
-import time
-from datetime import timedelta
+# import time
+# from datetime import timedelta
 from django.utils import timezone
-from django.utils.encoding import smart_str
-from captcha.conf import settings as captcha_settings
-from captcha.models import CaptchaStore, randrange, MAX_RANDOM_KEY
+# from django.utils.encoding import smart_str
+# from captcha.conf import settings as captcha_settings
+from captcha.models import CaptchaStore#, randrange, MAX_RANDOM_KEY
 from captcha.helpers import captcha_image_url
 
 from rest_framework import status
@@ -16,7 +16,7 @@ class GetCaptchaViewSet(GetViewSet):
 
     def list(self, request, *args, **kwargs):
 
-        captchas = CaptchaStore.objects.filter(expiration__lt=timezone.localtime()).delete()
+        CaptchaStore.objects.filter(expiration__lt=timezone.localtime()).delete()
         captcha_key = CaptchaStore.generate_key()
 
         return Response({'captcha_url': captcha_image_url(captcha_key)}, status=status.HTTP_200_OK)
