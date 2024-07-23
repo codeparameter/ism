4073a216c4af0e8bc0fbe843f71612bd2ed002d6
codeparameter <codeparameter.com@gmail.com>
2024-07-23 03:51:19 +0330

    captcha completed

--- maptcha/viewsets.py
+++ maptcha/viewsets.py
@@ -1,9 +1,6 @@
-# import time
-# from datetime import timedelta
 from django.utils import timezone
-# from django.utils.encoding import smart_str
-# from captcha.conf import settings as captcha_settings
-from captcha.models import CaptchaStore#, randrange, MAX_RANDOM_KEY
+from captcha.conf import settings as captcha_settings
+from captcha.models import CaptchaStore
 from captcha.helpers import captcha_image_url
 
 from rest_framework import status
@@ -15,8 +12,13 @@ class GetCaptchaViewSet(GetViewSet):
     permission_classes = ()
 
     def list(self, request, *args, **kwargs):
-
-        CaptchaStore.objects.filter(expiration__lt=timezone.localtime()).delete()
-        captcha_key = CaptchaStore.generate_key()
-
-        return Response({'captcha_url': captcha_image_url(captcha_key)}, status=status.HTTP_200_OK)
+        challenge, response = captcha_settings.get_challenge(None)()
+        expired_captchas = CaptchaStore.objects.filter(expiration__lt=timezone.localtime())
+        if expired_captchas:
+            cid = expired_captchas[0].id
+            expired_captchas[0].delete()
+            captcha = CaptchaStore.objects.create(id=cid, challenge=challenge, response=response)
+        else:
+            captcha = CaptchaStore.objects.create(challenge=challenge, response=response)
+            
+        return Response({'captcha_url': captcha_image_url(captcha.hashkey)}, status=status.HTTP_200_OK)
